6462ea9bd448ec0796d5805e5b85bd5ded6c48fc
waynejsk <juchuang123@gmail.com>
2020-09-18 17:13:18 +0900

    implement paginate func on users index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.find_or_create_by(email: 'admin@email.com') do |user|
+User.find_or_create_by!(email: 'admin@email.com') do |user|
   user.name = 'admin',
-  user.admin = ture,
+  user.admin = true,
   user.password = 'password',
   user.password_confirmation = 'password'
 end
+
+
+=begin
+(1..50).each do |i|
+  User.find_or_create_by!(email: "foo#{i}@email.com") do |user|
+    user.name = "user",
+    user.admin = false,
+    user.password = "p",
+    user.password_confirmation = "p"
+  end
+end
+=end
 
 (1..50).each do |i|
   Memo.create!(
